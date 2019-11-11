a33bc585674e58630558cadbd0f2c314396b186c
caesar <454451758@qq.com>
2019-11-11 21:45:29 +0800

    Init Commit

--- app/design/frontend/Restful/inferno/registration.php
+++
@@ -1,6 +0,0 @@
-<?php
-\Magento\Framework\Component\ComponentRegistrar::register(
-    \Magento\Framework\Component\ComponentRegistrar::THEME,
-    'frontend/Restful/inferno',
-    __DIR__
-);
---
+++ reveal-gulp/build/theme.conf.ts
@@ -0,0 +1,19 @@
+import { ThemeConfig } from '../packages'
+
+const themeConfig: ThemeConfig = {
+    default: {
+        name: 'preact',
+        area: 'frontend',
+        src: 'Restful/preact',
+        locale: 'en_US',
+        parent: '',
+        mode: 'production',
+        styles: 'less',
+        scripts: 'tsx',
+        files: [
+            'css/app.css'
+        ]
+    }
+}
+
+export default themeConfig
